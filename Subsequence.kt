3350f5f75902d4c13e13a7905d147c33c302ec5a
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-05-10 23:17:12 +0900

    Solve problem No.14002

--- Subsequence.kt
+++ Subsequence.kt
@@ -1,33 +1,57 @@
 /**
  * Subsequence.kt
- * Class used in problem No.[11053, 11722]
+ * Class used in problem No.[11053, 11722, 14002]
  */
 
 class Subsequence(private val sequence: IntArray) {
-    private var memoMaxIncreaseLengthAt = IntArray(sequence.size)
-    private var memoMaxDecreaseLengthAt = IntArray(sequence.size)
+    private var memoMaxLengthIn = IntArray(sequence.size)
+    private var memoIndexLengthIncreasedBy = IntArray(sequence.size)
+    private val memoMaxIncreaseElement = IntArray(sequence.size)
 
     fun getMaxIncreaseLength(): Int {
         for (i in 1 until sequence.size) {
             for (j in 0 until i) {
-                if (sequence[j] < sequence[i] && memoMaxIncreaseLengthAt[i] < memoMaxIncreaseLengthAt[j] + 1) {
-                    memoMaxIncreaseLengthAt[i] = memoMaxIncreaseLengthAt[j] + 1
+                if (sequence[j] < sequence[i] && memoMaxLengthIn[i] < memoMaxLengthIn[j] + 1) {
+                    memoMaxLengthIn[i] = memoMaxLengthIn[j] + 1
+                    memoIndexLengthIncreasedBy[i] = j
                 }
             }
         }
 
-        return memoMaxIncreaseLengthAt.max()!!
+        return memoMaxLengthIn.max()!!
     }
 
     fun getMaxDecreaseLength(): Int {
         for (i in 1 until sequence.size) {
             for (j in 0 until i) {
-                if (sequence[j] > sequence[i] && memoMaxDecreaseLengthAt[i] < memoMaxDecreaseLengthAt[j] + 1) {
-                    memoMaxDecreaseLengthAt[i] = memoMaxDecreaseLengthAt[j] + 1
+                if (sequence[j] > sequence[i] && memoMaxLengthIn[i] < memoMaxLengthIn[j] + 1) {
+                    memoMaxLengthIn[i] = memoMaxLengthIn[j] + 1
                 }
             }
         }
 
-        return memoMaxDecreaseLengthAt.max()!!
+        return memoMaxLengthIn.max()!!
+    }
+
+    fun printMaxIncrease() {
+        val maxIncrease = IntArray(getMaxIncreaseLength())
+
+        memoizeMaxIncreaseElement(memoMaxLengthIn.indexOf(maxIncrease.size))
+
+        for (i in maxIncrease.indices) {
+            print("${memoMaxIncreaseElement[i]} ")
+        }
+
+        println("")
+    }
+
+    private fun memoizeMaxIncreaseElement(index: Int) {
+        if (index == 0) {
+            return
+        } else {
+            memoMaxIncreaseElement[memoMaxLengthIn[index] - 1] = sequence[index]
+
+            return memoizeMaxIncreaseElement(memoIndexLengthIncreasedBy[index])
+        }
     }
 }
