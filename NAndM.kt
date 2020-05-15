c5d2a842ce8e167b73436f74be032ca39920b75c
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-05-16 07:19:21 +0900

    Solve problem No.15651, Replace println with BufferedWriter

--- NAndM.kt
+++ NAndM.kt
@@ -1,20 +1,25 @@
 /**
  * NAndM.kt
- * Class used in problem No.[15649]
- * [range] corresponds to N, [maxDepth] corresponds to M
+ * Class used in problem No.[15649..15651]
+ * range corresponds to N, maxDepth corresponds to M
  */
+
+import java.io.*
+import java.lang.System.out
 
 class NAndM(private val range: Int, private val maxDepth: Int) {
     private var isVisited = BooleanArray(range + 1)
     private var visitedNumber = IntArray(maxDepth)
+    private val bufferedWriter = BufferedWriter(OutputStreamWriter(out))
 
     private fun dfsPermutation(depth: Int) {
         if (depth == maxDepth) {
             for (element in visitedNumber) {
-                print("$element ")
+                bufferedWriter.write("$element ")
             }
 
-            println("")
+            bufferedWriter.newLine()
+            bufferedWriter.flush()
 
             return
         }
@@ -34,10 +39,11 @@ class NAndM(private val range: Int, private val maxDepth: Int) {
     private fun dfsCombination(depth: Int, minDepth: Int) {
         if (depth == maxDepth) {
             for (element in visitedNumber) {
-                print("$element ")
+                bufferedWriter.write("$element ")
             }
 
-            println("")
+            bufferedWriter.newLine()
+            bufferedWriter.flush()
 
             return
         }
@@ -54,10 +60,30 @@ class NAndM(private val range: Int, private val maxDepth: Int) {
         }
     }
 
+    private fun dfsRepetitivePermutation(depth: Int) {
+        if (depth == maxDepth) {
+            for (element in visitedNumber) {
+                bufferedWriter.write("$element ")
+            }
+
+            bufferedWriter.newLine()
+            bufferedWriter.flush()
+
+            return
+        }
+
+        for (i in 1..range) {
+            visitedNumber[depth] = i
+
+            dfsRepetitivePermutation(depth + 1)
+        }
+    }
+
     fun solveProblem(problemIndex: Int) {
         when (problemIndex) {
             1 -> dfsPermutation(0)
             2 -> dfsCombination(0, 1)
+            3 -> dfsRepetitivePermutation(0)
         }
     }
 }
